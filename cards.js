8dc18bf884464b734ee9a9140dbbcce13155e5a2
Welisio <63056611+Welisio@users.noreply.github.com>
2023-04-26 18:12:59 +0400

    1.1

--- cards.js
+++ cards.js
@@ -8,25 +8,24 @@ async function getCards ()  {
 async function appendCards () {
     cards = await axios.get(`https://fakestoreapi.com/products`)
     console.log(cards)
-    let container = document.querySelectorAll('.cards')
+    let container = document.querySelector('.cards')
     cards.data.forEach(element => {
-        container.insertAdjacentHTML('beforeend','1')
         container.insertAdjacentHTML('beforeend',`
             <div class="card-container">
-            <div class="category">Men's clothing</div>
-            <div class="title">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</div>
-            <img class="card-img" height="200px" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="">
+            <div class="category">${element.category}</div>
+            <div class="title">${element.title}</div>
+            <img class="card-img" height="200px" src="${element.image}" alt="">
             <div class="description">
-                Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday
+              ${element.description}
             </div>
             <div class="price-rate-count">
-                <div class="price">109.95$</div>
+                <div class="price">${element.price}$</div>
                 <div class="rate">
-                    <div>3.9</div> 
+                    <div>${element.rating.rate}</div> 
                     <i class="fa-solid fa-star"></i>
                 </div>
                 <div class="count">
-                    <div>120</div> 
+                    <div>${element.rating.count}</div> 
                     <i class="fa-solid fa-boxes-stacked"></i>
                 </div>
             </div>
